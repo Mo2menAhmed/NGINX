e1780d37799fb75320ef4a03ce29044b09bc2894
Mo2menAhmed <66696846+Mo2menAhmed@users.noreply.github.com>
2023-12-11 13:25:59 +0200

    Update install-K8s.sh

--- install-K8s.sh
+++ install-K8s.sh
@@ -18,7 +18,12 @@ sudo sysctl --system
 sleep 2
 apt-get update -y 
 sleep 1
-apt-get install -y apt-transport-https curl docker.io
+sudo apt update
+sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-archive-keyring.gpg
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install -y containerd.io docker-ce docker-ce-cli
 sleep 1
 cat > /etc/docker/daemon.json <<EOF
 {
@@ -35,6 +40,18 @@ systemctl daemon-reload
 systemctl restart docker
 systemctl enable docker
 sleep 1 
+sudo apt update
+sudo apt install git wget curl
+sleep 1 
+VER=$(curl -s https://api.github.com/repos/Mirantis/cri-dockerd/releases/latest|grep tag_name | cut -d '"' -f 4|sed 's/v//g')
+wget https://github.com/Mirantis/cri-dockerd/releases/download/v${VER}/cri-dockerd-${VER}.amd64.tgz
+tar xvf cri-dockerd-${VER}.amd64.tgz
+sudo mv cri-dockerd/cri-dockerd /usr/local/bin/
+wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/master/packaging/systemd/cri-docker.service
+wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/master/packaging/systemd/cri-docker.socket
+sudo mv cri-docker.socket cri-docker.service /etc/systemd/system/
+sudo sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
+sleep 1
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 sleep 1 
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
@@ -49,8 +66,11 @@ apt-get install -y kubelet kubeadm kubectl
 swapoff -a
 kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-bind-port=6443 > /kub.txt
 mkdir -p $HOME/.kube && cp -i /etc/kubernetes/admin.conf $HOME/.kube/config && sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-
 kubectl taint nodes --all node-role.kubernetes.io/master-
-kubectl apply -f https://docs.projectcalico.org/v3.8/manifests/calico.yaml
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.4/manifests/tigera-operator.yaml
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.4/manifests/custom-resources.yaml
+
 sleep 1
 echo "COMPLETED"
 #### FINISH 
